eee32a7c74b0f0fd94f99920bc0a5b956c1f2f33
AleynaImece <aleynaimece2002@gmail.com>
2024-07-01 19:44:51 +0300

    Mirror Test Scene Added

---
+++ Assets/Scripts/MirrorScripts/Game2_moveShipMirror.cs
@@ -0,0 +1,101 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game2_moveShipMirror : NetworkBehaviour
+{
+    public float baseSpeed = 0.6f;
+    public float speedIncrement = 0.6f;
+    private float currentSpeed = 0f;
+    private float maxSpeed = 5f;
+    private bool isMoving = false;
+    private CountdownController countdownController;
+
+    public GameOver2 gameOver2;
+
+
+    void Start()
+    {
+        //find countdown
+        countdownController = FindObjectOfType<CountdownController>();
+        if (countdownController == null)
+        {
+            Debug.LogError("CountdownController not found in the scene.");
+        }
+    }
+
+    void Update()
+    {
+        // wait until countdown finishes
+        if (countdownController != null && countdownController.IsCountdownFinished())
+        {
+            //screen control
+            if (Input.GetMouseButtonDown(0))
+            {
+                IncreaseSpeed();
+            }
+
+            // movement control
+            if (isMoving)
+            {
+                float translation = currentSpeed * Time.deltaTime;
+                transform.Translate(0, translation, 0);
+            }
+        }
+    }
+
+    void IncreaseSpeed()
+    {
+        if (countdownController.IsCountdownFinished()) 
+        {
+            isMoving = true;
+            currentSpeed += speedIncrement;
+
+            if (currentSpeed > maxSpeed)
+            {
+                currentSpeed = maxSpeed;
+            }
+        }
+    }
+
+    void StopMoving()
+    {
+        isMoving = false;
+        currentSpeed = 0f;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            GameOver();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        Debug.Log("Oyun Bitti!");
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameOver2.ShowGameOverScreen();
+        Time.timeScale = 0f;
+    }
+
+    /*[ClientRpc]
+    private void SpawnClient()
+    {
+        if ()
+    }*/
+}
+
+
+
